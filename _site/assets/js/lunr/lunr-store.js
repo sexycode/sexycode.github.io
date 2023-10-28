4e5e53de2c34d46dda4ed8614996d7734739bd4d
sexycode <sexycode@gmail.com>
2023-10-28 15:04:58 +0900

    _site 는 꼭 필요하네

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Android Source Version check",
+        "excerpt":"android version check 12/maincore/build/core/version_defaults.mk PLATFORM_SDK_VERSION := 31 , PLATFORM_VERSION_LAST_STABLE := 12 13/maincore/build/core/version_defaults.mk ``` This is the user-visible version. In a final release build it should be empty to use PLATFORM_VERSION as the user-visible version. For a preview release it can be set to a user-friendly value like 12 Preview 1...","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "/blog/android_version_check/",
+        "teaser": null
+      }]
